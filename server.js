ddc073a091d0d5fc56d3de40e6f93db428f443a2
Abdelrahman shaheen <a.shaheen20001@gmail.com>
2023-05-01 12:54:00 +0000

    movies-library

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const server = express();
+
+const datas = require('./Movie Data/data.json')
+
+const PORT = 3000;
+
+server.listen(PORT,()=>{
+
+    console.log("hay"+PORT);})
+
+    let movies=[];
+    function Format(title,poster_path,overview){
+        this.title=title;
+        this.poster_path=poster_path;
+        this.overview=overview;
+        movies.push(this);
+    }
+
+    let movie1 = new Format(datas.title,datas.poster_path,datas.overview);
+
+    server.get('/',(req,res)=>{
+        res.send(movie1);
+    })
+
+    server.get('/favorite',(req,res)=>{
+        res.send("Welcome to Favorite Page")
+    })
+
+
+
+    server.get("*",(req,res)=>{
+        res.status(404).send("page not found");
+    })
+
+    server.get('/error 500',(req,res)=>{
+        let obj={"status":500,"responseText":"Sorry, something went wrong"};
+        res.status(500).send(obj);
+    })
