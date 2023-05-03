99f11f3451fa0da47b65a4ef71ba2a9b4ab9a913
Abdelrahman shaheen <a.shaheen20001@gmail.com>
2023-05-03 17:46:45 +0000

    Api movies database

--- README.md
+++ README.md
@@ -4,20 +4,35 @@
 
 ## WRRC
 
-!["img wrrc"](./assets/Web%20WRRC.png)
+!["img wrrc"](./assets/APi.png)
 
 ## Overview
+
+ This page is movies libray , I used Api movies database from [The movie Database API](https://developers.themoviedb.org/3/getting-started/introduction).
+
+## Notes
+
+- I used Api for request and response in project.
+
+- I used  format clean code for my code.
 
 ## Getting Started
 
 1. clone this repository;
 2. run command "npm start" in terminal;
 3. go browser and write url "localhost:3000".
+4. add any path in project features (localhost:3000/path) to show the pages.
 
 ## Project Features
 
 1. in home page (path "/") you can show the movies(title and poster and overview),
 
-2. in favorite page (path '/favorite') you can show the favorite movies.
+2. in favorite page (path "/favorite") you can show the favorite movies.
 
-3. if you add any other path the error page not found is show .
+3. in page trending (path "/trending") you can show the request and response  trending page from database api;
+
+4. in page search (path "/search") you can show the request and response  search page from database api
+
+5. in page watch (path "/watch") you can show the request and response watch page from database api
+
+6. if you add any other path the error page not found is show .
--- server.js
+++ server.js
@@ -1,37 +1,150 @@
 const express = require("express");
 const server = express();
-
-const datas = require('./Movie Data/data.json')
-
+const cors = require("cors");
+const dataMovie = require("./Movie Data/data.json");
+server.use(cors());
+require('dotenv').config();
+const axios = require('axios');
+const apiKey=process.env.api_key;
 const PORT = 3000;
+let movies = [];
 
-server.listen(PORT,()=>{
+console.log("this "+" " +apiKey);
 
-    console.log("hay"+PORT);})
 
-    let movies=[];
-    function Format(title,poster_path,overview){
-        this.title=title;
-        this.poster_path=poster_path;
-        this.overview=overview;
-        movies.push(this);
+// let movie1 = new Format(datas.title, datas.poster_path, datas.overview);
+
+server.get("/", homePage);
+
+server.get("/trending",trending);
+server.get("/search",search);
+server.get("/discover",discover);
+server.get("/watch",watch)
+server.get("/favorite",favoritePage);
+
+server.get("/500", handlerError500);
+
+server.get("*", handlerDefaultErro);
+
+function homePage(req,res){
+    let singleMovie = new Format(dataMovie.title, dataMovie.poster_path, dataMovie.overview);
+    // let mapResult = dataMovie.map(item => {
+        
+    //     return singleMovie;
+    // });
+    res.send(singleMovie);
+}
+
+function favoritePage(req,res){
+    res.send("Welcome to Favorite Page");
+}
+
+function trending(req,res){
+    try {
+        const url=`https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&language=en-US`;
+    axios.get(url)
+    .then(result=>{let resultAxois=result.data.results.map(item=>{
+        let singleMovie = new Format(item.id,item.title,item.release_date,item.poster_path,item.overview);
+        return singleMovie;
+    })
+    
+        res.send(resultAxois);
+    }).catch((error)=>{
+        console.log('Try again somthing happend',error)
+        res.status(500).send(error);
+    })
+}
+catch(error){
+    errorHandler(error,req,res)
+}
+}
+ function search(req,res){
+    const url=`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=The&page=2`;
+    try {
+     axios.get(url)
+     .then(result=>{let resultAxois=result.data.results.map(item=>{
+        let singleMovie = new Format(item.id,item.title,item.release_date,item.poster_path,item.overview);
+        return singleMovie;
+    })
+    res.send(resultAxois);
+}    
+
+    
+    ).catch((error)=>{
+        console.log('Try again somthing happend',error)
+        res.status(500).send(error);
+    })
+    
+    } catch (error) {
+        errorHandler(error,req,res)
+    } 
+}
+function discover(req,res){
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false`;
+    try{
+        axios.get(url)
+        .then(result=>{let resultAxois=result.data.results.map(item=>{
+            let singleMovie = new Format(item.id,item.title,item.release_date,item.poster_path,item.overview);
+            return singleMovie;
+            })
+
+            res.send(resultAxois);
+        })
+        .catch((error)=>{
+            console.log('Try again somthing happend',error)
+            res.status(500).send(error);
+        })
     }
+    catch(error){
+        errorHandler(error,req,res)
+    }
+}
 
-    let movie1 = new Format(datas.title,datas.poster_path,datas.overview);
+function watch(req,res){
+    const url = `https://api.themoviedb.org/3/watch/providers/tv?api_key=${apiKey}&language=en-US`;
+    try{
+        axios.get(url)
+        .then(result=>{let resultAxois=result.data.results.map(item=>{
+            return item.display_priorities;
+            })
 
-    server.get('/',(req,res)=>{
-        res.send(movie1);
-    })
+            res.send(resultAxois);
+        })
+        .catch((error)=>{
+            console.log('Try again somthing happend',error)
+            res.status(500).send(error);
+        })
+    }
+    catch(error){
+        errorHandler(error,req,res)
+    }
+}
 
-    server.get('/favorite',(req,res)=>{
-        res.send("Welcome to Favorite Page")
-    })
+function handlerError500(req,res){
+    let obj = { status: 500, responseText: "Sorry, something went wrong" };
+    res.status(500).send(obj);
+}
 
-    server.get('/500',(req,res)=>{
-        let obj={"status":500,"responseText":"Sorry, something went wrong"};
-        res.status(500).send(obj);
-    })
+function errorHandler(error,req,res){
+    const err = {
+        status: 500,
+        message: error
+    }
+    res.status(500).send(err);
+}
+function handlerDefaultErro(req,res){
+    res.status(404).send("page not found");
+}
 
-    server.get("*",(req,res)=>{
-        res.status(404).send("page not found");
-    })
+function Format(id,title, release_date,poster_path, overview) {
+    this.id=id;
+  this.title = title;
+  this.release_date = release_date;
+  this.poster_path=poster_path;
+  this.overview = overview;
+  movies.push(this);
+}
+
+server.listen(PORT, () => {
+    console.log("hays" + PORT);
+  });
