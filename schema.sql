4699e474d547598aae31e7d87e4e74e6163f6935
Abdelrahman shaheen <a.shaheen20001@gmail.com>
2023-05-11 16:22:49 +0000

    postgres

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS infoMovies(
 
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    overView VARCHAR(255)
+    overView VARCHAR(255),
+    poster_path VARCHAR(255)
  
 );
